be421222ed35fb8b4dca3d0036e1f80e73dcb961
Philip <philipwood0912@gmail.com>
2020-01-28 17:30:38 -0500

    vue update

--- public/js/main_vm.js
+++ public/js/main_vm.js
@@ -1,16 +1,46 @@
 // imports always go first - if we're importing anything
 
+import ChatMessage from "./modules/ChatMessage.js";
+
 const socket = io();
 
-function setUserId(packet) {
-    debugger;
-    console.log(packet);
+function setUserId(sID, message) {
+    //debugger;
+    vm.socketID = sID;
 }
 
 function userDC(packet) {
     debugger;
     console.log(packet);
 }
+//main vue instance
+const vm = new Vue({
+    data: {
+        socketID: "",
+        messages: [],
+        message: "",
+        nickName: ""
+    },
+    methods: {
+        dispatchMessage() {
+            //emit a message event and send to the server
+            console.log('handle send message');
+
+            socket.emit('chat_message', {
+                content: this.message,
+                name: this.nickName || 'anonymous'
+            })
+        }
+    },
+    components: {
+        newmessage: ChatMessage
+    },
+    mounted: function() {
+        console.log('mounted');
+    }
+}).$mount('#app');
+
+
 //some event handling -> these events are coming from the server
 socket.addEventListener('connected', setUserId);
 socket.addEventListener('user_disconnect', userDC);
